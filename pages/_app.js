4801ca953fc9c5a952a95c23c6f95c624ad66f7f
Ali Mirzaei <98237451+Ali-mirzayi@users.noreply.github.com>
2024-06-01 03:47:32 +0330

    Update meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -20,15 +20,16 @@ function MyApp(props) {
   return (
     <>
     <Head>
-      <meta name="author" content="Ali mirzaei zadeh"/>
-      <meta name="description" content="Amin Khorasani"/>
-      <meta name="keywords" content="3Dmax, Vray, PhotoShop"/>
-      <meta name="keywords" content="Amin, Mohammad, Khorasani"/>
-      <meta name="keywords" content="خراسانی, امین, محمد"/>
-      <meta name="keywords" content="تری دی مکس, طراحی کابینت, طراحی آشپزخونه,طراحی آشپز خانه"/>
-      <meta name="keywords" content="طراحی دکوراسیون,دکوراسیون داخلی,طراح دکوراسیون,طراح دکور داخلی,دکوراسیون,دکوری"/>
-      <meta name="keywords" content="طراحی,تریدی ارتیست,سی جی ارتیست,سه بعدی,طراحی کمد دیواری,طراحی سرویس بهداشتی,جاکفشی"/>
-      <meta name="keywords" content="دیوار,کابینت,آشپزخانه,آشپزی,خواب"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Amin Khorasani">
+    <title>Amin Khorasani</title>
+    <meta name="description" content="محمد امین خراسانی هستم. یک 3D Artist، طراح دکوراسیون داخلی، طراح کابینت خود آموخته هستم. مدت زمان تحویل طراحی دکوراسیون داخلی و طراحی کابینت بستگی به متراژ و سبک کاری که میخواهید متغیر است. برای اطلاع از قیمت ها و مشاوره رایگان، میتوانید با من در شبکه های اجتماعی و شماره تماس های زیر در تماس باشید.">
+    <meta property="og:url" content="https://aminkhorasani.netlify.app">
+    <meta property="og:title" content="Amin Khorasani" key="title">
+    <meta property="og:description" content="سلام! من محمد امین خراسانی هستم. یک 3D Artist، طراح دکوراسیون داخلی و طراح کابینت هستم. خودآموز و مسلط به نرم افزارهای مربوطه می باشم. برای اطلاعات بیشتر، می توانید با من در شبکه های اجتماعی و شماره تماس های زیر در تماس باشید.">
+    <meta name="keywords" content="امین خراسانی, amin khorasani, aminkhorasani , 3D Artist, طراح دکوراسیون داخلی, طراح کابینت">
+    <link rel="icon" type="image/png" sizes="16x16" href="/icon-192x192.png" />
     </Head>
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={isDarkTheme ? createTheme(darkTheme) : createTheme(lightTheme)}>
